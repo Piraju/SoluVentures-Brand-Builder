d855d3090e45f388116d2b343b735ec7572b5358
Renato Piraju <piraju@gmail.com>
2026-03-13 05:59:36 -0300

    Delete scr directory

--- scr/main.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { LandingPage } from './LandingPage' // Sem o caminho "/src", pois já estão na mesma pasta
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <LandingPage />
-  </React.StrictMode>,
-)
